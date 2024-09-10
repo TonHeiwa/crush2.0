fa0564ef5f6d37c932f26289fc7eca784a4aedf3
TonHeiwa <wvrtncordeiro@gmail.com>
2024-09-10 00:12:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "You got Rickrolled 😘 (FOLLOW ME @kristel_tech or 7 years bad luck)";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  question.innerHTML = "agora vai ter que casar comigo hihih";
+  gif.src = "https://tenor.com/pt-BR/view/aladdin-swoon-love-gif-7391211";
 });
 
 // Make the No button move randomly on hover
